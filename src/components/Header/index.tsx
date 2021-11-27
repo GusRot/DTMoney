df3565b43d4799f4de100a5fda2f0cc55f566630
Gustavo Rotta <gustavo.rotta@hotmail.com>
2021-11-27 16:12:57 -0300

    modal setup

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,25 +1,33 @@
 import logoImg from '../../assets/logo.svg';
 import { Container } from './styles';
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from 'react-modal';
 
 
 export function Header() {
 
-//  const[modalOpen, setModalOpen] = useState(false);
+ const[modalOpen, setModalOpen] = useState(false);
 
-//  function handleModalOpen() {
-//     setModalOpen(true);
-//  }
+ function handleModalOpen() {
+    setModalOpen(true);
+ }
 
-//  function handleModalClose() {
-//     setModalOpen(false)
-//  }
+ function handleModalClose() {
+    setModalOpen(false);
+ }
 
     return(
         <Container>
             <div className="container">
                 <img src={logoImg} alt="DT Money" />
-                <button type = "button">Nova Transação</button>
+                <button type = "button" onClick = {handleModalOpen}>Nova Transação</button>
+
+                <Modal 
+                isOpen = {modalOpen}
+                onRequestClose = {handleModalClose}
+                >
+                    <h2>Cadastrar</h2>
+                </Modal>
             </div>
         </Container>
     )
