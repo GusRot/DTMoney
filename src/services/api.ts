3af1a2c3e213a6b4169cfa82382e45bf8bd0cc20
Gustavo Rotta <gustavo.rotta@hotmail.com>
2021-12-05 23:48:16 -0300

    build

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 // production url: "http://192.168.0.16:8080/api"
+// production vercel url: "https://dt-money-ch432zwtn-gusrot.vercel.app/"
+// dev url: "http://localhost:3000/api"
 
 export const api = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: "https://dt-money-ch432zwtn-gusrot.vercel.app/",
 });
