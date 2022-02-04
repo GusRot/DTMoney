09400340ad4bb4d986ab7b7fbc3c9fffe9750cce
Gustavo Rotta <gustavo.rotta@hotmail.com>
2022-02-04 17:30:05 -0300

    readme

--- src/components/TransactionModal/styles.ts
+++ src/components/TransactionModal/styles.ts
@@ -1,20 +1,18 @@
 import styled from "styled-components";
-import {transparentize} from 'polished';
+import { transparentize } from "polished";
 
 export const Container = styled.form`
-
     h2 {
         color: grey;
         font-size: 2rem;
         margin-bottom: 2.3rem;
-
     }
 
     input {
-        width:100%;
+        width: 100%;
         padding: 0 0.2rem;
         height: 4rem;
-        border: 1px solid #D7D7D7;
+        border: 1px solid #d7d7d7;
         background: #e7e9ee;
         border-radius: 0.4rem;
         margin-bottom: 1.2rem;
@@ -22,12 +20,11 @@ export const Container = styled.form`
         &::placeholder {
             color: black;
         }
-
     }
 
     button {
-        background-color: ${transparentize(0.6, 'green')};
-        width:100%;
+        background-color: ${transparentize(0.6, "green")};
+        width: 100%;
         padding: 0 0.2rem;
         height: 4rem;
         border-radius: 0.4rem;
@@ -37,11 +34,10 @@ export const Container = styled.form`
     }
 
     input + button:hover {
-        background-color: ${transparentize(0.4, 'green')};
+        background-color: ${transparentize(0.4, "green")};
     }
-`
+`;
 export const ContainerButton = styled.div`
-
     margin: 1.2rem 0;
     display: grid;
     grid-template-columns: 1fr 1fr;
@@ -49,13 +45,13 @@ export const ContainerButton = styled.div`
 
     button {
         height: 5rem;
-        border: 1px solid #D7D7D7;
+        border: 1px solid #d7d7d7;
         background: transparent;
         display: flex;
         align-items: center;
-        justify-content:center;
+        justify-content: center;
         gap: 1.5rem;
-        color: black; 
+        color: black;
         padding: 0 1rem;
         cursor: pointer;
         transition: filter 0.2s;
@@ -63,7 +59,7 @@ export const ContainerButton = styled.div`
         &:hover {
             filter: brightness(0.8);
         }
-        
+
         img {
             width: 2rem;
             height: 2rem;
@@ -71,11 +67,10 @@ export const ContainerButton = styled.div`
     }
 
     .active {
-    
-        background-color: ${transparentize(0.8, 'red')};
-    
-        &:first-child { 
-            background-color: ${transparentize(0.7, 'green')};
+        background-color: ${transparentize(0.8, "red")};
+
+        &:first-child {
+            background-color: ${transparentize(0.7, "green")};
         }
     }
-    `
+`;
